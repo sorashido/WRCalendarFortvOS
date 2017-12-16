008ceb5a22e462f94cac8f453d9941da78f5b2c0
shido <poporotylm@gmail.com>
2017-12-16 11:35:56 +0900

    first commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+
+this repository is based on WRCalendarView(https://github.com/wayfinders/WRCalendarView)
---
+++ WRCalendarFortvOS/WRCalendarFortvOS.h
@@ -0,0 +1,19 @@
+//
+//  WRCalendarFortvOS.h
+//  WRCalendarFortvOS
+//
+//  Created by Daichi Shibata on 2017/12/16.
+//  Copyright © 2017 daichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WRCalendarFortvOS.
+FOUNDATION_EXPORT double WRCalendarFortvOSVersionNumber;
+
+//! Project version string for WRCalendarFortvOS.
+FOUNDATION_EXPORT const unsigned char WRCalendarFortvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WRCalendarFortvOS/PublicHeader.h>
+
+
